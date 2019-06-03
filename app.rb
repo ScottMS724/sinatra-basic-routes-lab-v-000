23202c5d509ce921d0fee92d1a1970100e1362cc
Scott Silverman <scottms724@gmail.com>
2019-06-03 19:47:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,5 +4,13 @@ class App < Sinatra::Base
   
   get "/name" do 
     "My name is Scott."
+  end 
+  
+  get "/hometown" do 
+    "My hometown is Syosset."
+  end 
+  
+  get "/favorite-song" do 
+    "My favorite song is Mockingbird."
   
 end
